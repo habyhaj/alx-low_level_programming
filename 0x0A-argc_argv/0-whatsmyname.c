f42660004f45f774d1c9f5a98d61cd8c2a4a3f99
habyhaj <habyhaj07@gmail.com>
2023-05-12 21:41:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of programme
+ * @argv: argument
+ * @argc: string
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
