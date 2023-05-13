2dacd86b0d5a327792f1c2782a171b06d7d1980c
habyhaj <habyhaj07@gmail.com>
2023-05-12 18:56:01 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 fibonacci numbers, starting with 1 and 2
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
  * Return: Always 0 (Success)
  */
@@ -15,15 +15,14 @@ int main(void)
 
 	for (i = 1; i <= 50; ++i)
 	{
-		if (j != 2044910807)
+		if (j != 20365011074)
 		{
-			printf("%1d, ", j);
+			printf("%ld, ", j);
 		} else
 		{
-			printf("%1d\n", j);
+			printf("%ld\n", j);
 		}
 		next = j + k;
-		j = k;
 		j = k;
 		k = next;
 	}
